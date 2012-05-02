065cab5355fc528d58d62aba427215e30e119b75
chandrasekarMariappan <chandrasekar.m@payoda.com>
2012-05-02 11:36:18 +0530

    Update ModalDialog/js/ModalDialog.js

--- ModalDialog/js/ModalDialog.js
+++ ModalDialog/js/ModalDialog.js
@@ -20,8 +20,9 @@
     var title;
     var header;
     var modalEvents;
+    var eventHandlerIndex = 1;
     $.fn.Dialog = function (settings) {
-    var configs = {
+        var configs = {
             'body': 'message',
             'header': 'header',
             'title': 'title',
@@ -34,11 +35,11 @@
         if (configs["buttons"] != undefined) {
             modalEvents = "<script type='text/javascript'>";
             $.each(configs["buttons"], function (key, value) {
-            modalFooter += "<a href='#'  class='btn btn-primary' data-dismiss='modal' id=Sample" + key + " >" + key + "</a>";
-                modalEvents += "$('#Sample" + key + "').click(" + value + ");";
+                modalFooter += "<a href='#'  class='btn btn-primary' data-dismiss='modal' id=Sample" + eventHandlerIndex + key + " >" + key + "</a>";
+                modalEvents += "$('#Sample" + eventHandlerIndex + key + "').click(" + value + ");";
             });
             modalEvents += "</script>";
-            $("body").append(modalEvents);
+            eventHandlerIndex += 1;
         }
         modalFooter += "</div>";
         message = configs['body'];
@@ -48,7 +49,7 @@
             $("body").append("<div id='sample' title='" + title + "' style='display: none' class='modal fade'><div class='modal-header' id='mod-header'><a class='close' data-dismiss='modal' title='Close'><i class='icon-remove'></i></a><h3>                </h3>        </div>        <div class='modal-body' id='mod-body'>            <p>                            </p>        </div><div class='modal-footer'> " + modalFooter + "</div>");
             isfirstTime = false;
         }
-        $("body").append(modalEvents);
+        $("div").append(modalEvents);
         $('#mod-body').html(message);
         $('#mod-header').html("<a class='close' data-dismiss='modal' title='Close'><i class='icon-remove'></i></a><h3>" + header + "</h3>");
         $('#sample').attr('title', title);
